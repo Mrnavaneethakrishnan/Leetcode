68d0636fbfec8e8df1bff4aeace059e55684002d
Navaneethakrishnan .S <143734607+Mrnavaneethakrishnan@users.noreply.github.com>
2024-08-04 11:26:16 +0530

    Time: 182 ms (18.92%), Space: 70.5 MB (21.62%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
@@ -1,19 +1,26 @@
+
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
-        List<Integer> arr = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            int prefix = 0;
-            for (int j = i; j < n; j++) {
-                prefix += nums[j];
-                arr.add(prefix);
+        List<Integer> subarraySum = new ArrayList<>();
+        
+        // Calculating sum of subarrays
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                subarraySum.add(sum);
             }
         }
-        Collections.sort(arr);
-        int result = 0;
-        int MOD = 1000000007;
+        
+        // Sort the subarray sums
+        Collections.sort(subarraySum);
+        
+        // Return the result
+        long result = 0;
+        int mod = (int)(1e9 + 7);
         for (int i = left - 1; i < right; i++) {
-            result = (result + arr.get(i)) % MOD;
+            result = (result + subarraySum.get(i)) % mod;
         }
-        return result;
+        return (int)result;
     }
 }
