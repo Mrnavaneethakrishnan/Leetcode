fdf5ecd8fd67157a6f3df05bc55556986d07183a
Navaneethakrishnan .S <143734607+Mrnavaneethakrishnan@users.noreply.github.com>
2024-08-16 07:48:16 +0530

    Time: 36 ms (29.69%), Space: 44.4 MB (63.79%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,38 +1,40 @@
 class Solution {
-    public int maxPoints(int[][] p) {
-        if (p.length == 1) return 1;
-        int max = 2;
-        for (int i = 0; i < p.length - 1; i++) {
-            int x1 = p[i][0], y1 = p[i][1];
-            for (int j = i + 1; j < p.length; j++) {
-                int x2 = p[j][0], y2 = p[j][1];
-                int count = 2;
-                if (x2 == x1) {
-                    for (int l = 0; l < p.length; l++) {
-                        if (l == i || l == j) continue;
-                        int x3 = p[l][0];
-                        if (x3 == x1) count++;
-                    }
-                } else if (y2 == y1) {
-                    for (int l = 0; l < p.length; l++) {
-                        if (l == i || l == j) continue;
-                        int y3 = p[l][1];
-                        if (y3 == y2) count++;
-                    }
-                } else {
-                    double k = (y2 * 1.0 - y1) / (x2 - x1);
-                    double b = y1 - k * x1;
-                    for (int l = 0; l < p.length; l++) {
-                        if (l == i || l == j) continue;
-                        int x3 = p[l][0], y3 = p[l][1];
-                        double yPotential = k * x3 + b;
-                        yPotential = Math.round(yPotential * 10000.0) / 10000.0;
-                        if (y3 == yPotential) count++;
-                    }
-                }
-                if (max < count) max = count;
-            }
+    public int gcd(int a,int b){
+        if(a==0){
+            return b;
         }
-        return max;
+        return gcd(b%a,a);
+    }
+    
+    public int maxPoints(int[][] points) {
+        int res=0,n=points.length;
+        if (points.length<=2) return points.length;
+
+        for(int i=0;i<n;i++){
+
+            HashMap<String,Integer> slopCount=new HashMap<>();
+            int duplicate = 0;
+            int  max= 0;
+            for(int j=i+1;j<n;j++){
+                int x=points[j][0]-points[i][0];
+                int y=points[j][1]-points[i][1];
+                if (x==0&&y==0){
+        			duplicate++;
+        			continue;
+        		}
+                int g=gcd(x,y);
+                int dx=x/g;
+                int dy=y/g;
+                String s=dx+"/"+dy; 
+                
+                slopCount.put(s,slopCount.getOrDefault(s,0)+1);
+
+                max=Math.max(max,slopCount.get(s));
+            }
+            res=Math.max(max+duplicate+1,res);
+        }
+        
+        return res;
+
     }
 }
