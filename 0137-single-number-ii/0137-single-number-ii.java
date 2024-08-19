2c60a85a8cfed34988daa55d579ac0a798cb40d5
Navaneethakrishnan .S <143734607+Mrnavaneethakrishnan@users.noreply.github.com>
2024-08-19 12:26:15 +0530

    Time: 91 ms (5.13%), Space: 45.1 MB (88.9%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,22 +1,16 @@
-class Solution 
-{
-    public int singleNumber(int[] nums) 
-    {
-        for(int i=0;i<nums.length;i++)
-        {
-            int count=0;
-            for(int j=0;j<nums.length;j++)
-            {
-                if(nums[i]==nums[j])
-                {
-                   count++;
-                }
-            }
-            if(count==1)
-            {
-                return nums[i];
+class Solution {
+    public int singleNumber(int[] nums) {
+      for(int i=0;i<nums.length;i++){
+        int count=0;
+        for(int j=0;j<nums.length;j++){
+            if(i!=j && nums[i]==nums[j]){
+                count++;
             }
         }
-        return 0;
+        if(count==0){
+            return nums[i];
+        }
+      }
+      return -1;
     }
 }
