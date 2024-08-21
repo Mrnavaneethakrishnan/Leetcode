409d37c9817e49c59d7f54087c6e492c5f96e84c
Navaneethakrishnan .S <143734607+Mrnavaneethakrishnan@users.noreply.github.com>
2024-08-21 15:18:28 +0530

    Time: 2 ms (99.11%), Space: 42.7 MB (83.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,24 +1,29 @@
-class Solution {
-    public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-        	return true;
-        }
-        int start = 0;
-        int last = s.length() - 1;
-        while(start <= last) {
-        	char currFirst = s.charAt(start);
-        	char currLast = s.charAt(last);
-        	if (!Character.isLetterOrDigit(currFirst )) {
-        		start++;
-        	} else if(!Character.isLetterOrDigit(currLast)) {
-        		last--;
-        	} else {
-        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-        			return false;
-        		}
-        		start++;
-        		last--;
-        	}
+class Solution 
+{
+    public boolean isPalindrome(String s) 
+    {
+        int n= s.length();
+        int i= 0;
+        int j= n-1;
+        while(i <= j)
+        {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) 
+            {
+                i++;
+            }
+            
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) 
+            {
+                j--;
+            }
+            int c1= Character.toLowerCase(s.charAt(i));
+            int c2= Character.toLowerCase(s.charAt(j));
+            if(c1 != c2)
+            {
+                return false;
+            }
+            i++; 
+            j--;
         }
         return true;
     }
