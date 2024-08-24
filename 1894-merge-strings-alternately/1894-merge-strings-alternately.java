af134e9bf267663ba982641baf66a2bbf9f6674d
Navaneethakrishnan .S <143734607+Mrnavaneethakrishnan@users.noreply.github.com>
2024-08-24 14:39:17 +0530

    Time: 7 ms (22.04%), Space: 42.7 MB (19.79%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public String mergeAlternately(String word1, String word2) 
+    {
+        String s="";
+        for(int i=0;i<word1.length() || i<word2.length();i++)
+        {
+            if(i<word1.length())
+            {
+                s=s+word1.charAt(i);
+            }
+             if(i<word2.length())
+            {
+                s=s+word2.charAt(i);
+            }
+        }
+        return s;
+    }
+}
